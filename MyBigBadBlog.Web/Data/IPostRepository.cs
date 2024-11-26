e26c0022be1484df11613bb664a4e367f8ac83c1
Bruno Candia <brunocandia@gmail.com>
2024-11-26 12:27:27 -0300

    Add project files.

---
+++ MyBigBadBlog.Web/Data/IPostRepository.cs
@@ -0,0 +1,22 @@
+﻿namespace MyBigBadBlog.Web.Data
+{
+    public interface IPostRepository
+    {
+        Task<IEnumerable<(PostMetadata, string)>> GetPostsAsync(int count, int page);
+
+        Task<(PostMetadata, string)> GetPostAsync(string slug);
+    }
+
+    public record PostMetadata(string Filename, string Title, string Author, DateTime Date)
+    {
+        public string Slug => Uri.EscapeDataString(Title.ToLower());
+
+        //public string Slug
+        //{
+        //    get
+        //    {
+        //        return Uri.EscapeDataString(Title.ToLower());
+        //    }
+        //}
+    }
+}
---
+++ MyBigBadBlog.Web/Data/PostRepository.cs
@@ -0,0 +1,16 @@
+﻿
+namespace MyBigBadBlog.Web.Data
+{
+    public class PostRepository : IPostRepository
+    {
+        public Task<(PostMetadata, string)> GetPostAsync(string slug)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<(PostMetadata, string)>> GetPostsAsync(int count, int page)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
